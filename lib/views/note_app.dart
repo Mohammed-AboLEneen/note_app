2310e1f20c1d79dcd1b2de0590f5f27249791136
Mohammed-AboLEneen <mohmedhty1@gmail.com>
2023-05-29 09:36:20 +0300

    refactor the code

--- lib/views/note_app.dart
+++ lib/views/note_app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/note_alert.dart';
 import '../widgets/note_item.dart';
 import '../widgets/search_icon.dart';
 
@@ -55,90 +56,6 @@ class NoteView extends StatelessWidget {
   }
 }
 
-class WriteNoteContent extends StatelessWidget {
-  final TextEditingController controller = TextEditingController();
 
-  WriteNoteContent({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return AlertDialog(
-      backgroundColor: const Color(0xff303030),
-      content: SizedBox(
-        width: 350,
-        height: 350,
-        child: Padding(
-          padding: const EdgeInsets.only(top: 8.0),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                TextField(
-                  textInputAction: TextInputAction.next,
-                  decoration: InputDecoration(
-                      hintText: 'Title',
-                      hintStyle: TextStyle(color: Colors.white.withOpacity(.5)),
-                      enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20),
-                          borderSide:
-                              BorderSide(color: Colors.white.withOpacity(.5))),
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20),
-                          borderSide: const BorderSide(color: Colors.teal))),
-                ),
-                const SizedBox(height: 15),
-                TextField(
-                  textInputAction: TextInputAction.next,
-                  controller: controller,
-                  textAlign: TextAlign.start,
-                  decoration: InputDecoration(
-                      contentPadding: const EdgeInsets.symmetric(
-                          horizontal: 20, vertical: 110.0),
-                      hintText: 'Content',
-                      hintStyle: TextStyle(color: Colors.white.withOpacity(.5)),
-                      enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20),
-                          borderSide:
-                              BorderSide(color: Colors.white.withOpacity(.5))),
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20),
-                          borderSide: const BorderSide(color: Colors.teal))),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-      actions: [
-        ElevatedButton(
-          onPressed: () => Navigator.pop(context),
-          style: ElevatedButton.styleFrom(
-            backgroundColor:
-                Colors.teal, // Set the background color of the button
-          ),
-          child: const Text('Create'),
-        ),
-      ],
-    );
-  }
-}
 
-class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return TextField(
-      textInputAction: TextInputAction.next,
-      decoration: InputDecoration(
-          hintText: 'Title',
-          hintStyle: TextStyle(color: Colors.white.withOpacity(.5)),
-          enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(20),
-              borderSide: BorderSide(color: Colors.white.withOpacity(.5))),
-          focusedBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(20),
-              borderSide: const BorderSide(color: Colors.teal))),
-    );
-  }
-}
