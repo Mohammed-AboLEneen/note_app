8508c074b8ff267f6ab1937533bef39d21d67c0d
Mohammed-AboLEneen <mohmedhty1@gmail.com>
2023-05-29 09:32:30 +0300

    add FloatingActionButton show AlertDialog

--- lib/main.dart
+++ lib/main.dart
@@ -19,20 +19,22 @@ class MyApp extends StatelessWidget {
         // TRY THIS: Try running your application with "flutter run". You'll see
         // the application has a blue toolbar. Then, without quitting the app,
         // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: const ColorScheme.light(),
-        useMaterial3: true,
-      ),
-      home: const NoteView(),
+          // and then invoke "hot reload" (save your changes or press the "hot
+          // reload" button in a Flutter-supported IDE, or press "r" if you used
+          // the command line to start the app).
+          //
+          // Notice that the counter didn't reset back to zero; the application
+          // state is not lost during the reload. To reset the state, use hot
+          // restart instead.
+          //
+          // This works for code too, not just values: Most code changes can be
+          // tested with just a hot reload.
+          colorScheme: const ColorScheme.light(
+              primary: Colors.white, onPrimary: Colors.white),
+          useMaterial3: true,
+          fontFamily: 'Poppins'),
+      home: NoteView(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
--- lib/views/note_app.dart
+++ lib/views/note_app.dart
@@ -1,16 +1,144 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../widgets/note_item.dart';
+import '../widgets/search_icon.dart';
+
 class NoteView extends StatelessWidget {
-  const NoteView({Key? key}) : super(key: key);
+  NoteView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-      appBar: AppBar(
-        title: const Text('Note App', style: TextStyle(fontSize: 20),),
+      backgroundColor: const Color(0xff303030),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 40,
+            ),
+            const Row(children: [
+              Text(
+                'Notes',
+                style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+              Spacer(),
+              IconSearch(),
+            ]),
+            const SizedBox(
+              height: 10,
+            ),
+            Expanded(
+                child: ListView.builder(
+              itemBuilder: (context, index) => ItemNote(),
+              itemCount: 10,
+            ))
+          ],
+        ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return WriteNoteContent();
+            },
+          );
+        },
+        backgroundColor: const Color(0xff52EFD8),
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class WriteNoteContent extends StatelessWidget {
+  final TextEditingController controller = TextEditingController();
+
+  WriteNoteContent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      backgroundColor: const Color(0xff303030),
+      content: SizedBox(
+        width: 350,
+        height: 350,
+        child: Padding(
+          padding: const EdgeInsets.only(top: 8.0),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                TextField(
+                  textInputAction: TextInputAction.next,
+                  decoration: InputDecoration(
+                      hintText: 'Title',
+                      hintStyle: TextStyle(color: Colors.white.withOpacity(.5)),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                          borderSide:
+                              BorderSide(color: Colors.white.withOpacity(.5))),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                          borderSide: const BorderSide(color: Colors.teal))),
+                ),
+                const SizedBox(height: 15),
+                TextField(
+                  textInputAction: TextInputAction.next,
+                  controller: controller,
+                  textAlign: TextAlign.start,
+                  decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 110.0),
+                      hintText: 'Content',
+                      hintStyle: TextStyle(color: Colors.white.withOpacity(.5)),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                          borderSide:
+                              BorderSide(color: Colors.white.withOpacity(.5))),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                          borderSide: const BorderSide(color: Colors.teal))),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      actions: [
+        ElevatedButton(
+          onPressed: () => Navigator.pop(context),
+          style: ElevatedButton.styleFrom(
+            backgroundColor:
+                Colors.teal, // Set the background color of the button
+          ),
+          child: const Text('Create'),
+        ),
+      ],
+    );
+  }
+}
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return TextField(
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+          hintText: 'Title',
+          hintStyle: TextStyle(color: Colors.white.withOpacity(.5)),
+          enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20),
+              borderSide: BorderSide(color: Colors.white.withOpacity(.5))),
+          focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20),
+              borderSide: const BorderSide(color: Colors.teal))),
     );
   }
 }
