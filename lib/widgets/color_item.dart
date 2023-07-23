a7f421978200bde0a2013ed18d9c70ee1040a059
mohmed <mohmedhty1@gmail.com>
2023-07-24 01:28:17 +0300

    change delete way and add edit feature, select and change the color of note

--- lib/widgets/color_item.dart
+++ lib/widgets/color_item.dart
@@ -1,20 +1,45 @@
 import 'package:flutter/cupertino.dart';
 
-class ColorItem extends StatelessWidget {
-  final int color;
+class ColorItem extends StatefulWidget {
+  final Color color1;
+  final Color color2;
+  final int mainColor;
 
-  const ColorItem({super.key, required this.color});
+  const ColorItem(this.color1, this.color2,
+      {super.key, required this.mainColor});
 
+  @override
+  State<ColorItem> createState() => _ColorItemState();
+}
+
+class _ColorItemState extends State<ColorItem> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
+
     return Padding(
-      padding: const EdgeInsets.only(right: 8.0),
-      child: Container(
-        height: 55,
-        width: 55,
-        decoration: BoxDecoration(shape: BoxShape.circle, color: Color(color)),
-      ),
-    );
+        padding: const EdgeInsets.only(right: 8.0),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            TweenAnimationBuilder(
+                tween: ColorTween(begin: widget.color1, end: widget.color2),
+                duration: const Duration(seconds: 1),
+                builder: (_, value, __) {
+                  return Container(
+                    height: 65,
+                    width: 65,
+                    decoration:
+                        BoxDecoration(color: value, shape: BoxShape.circle),
+                  );
+                }),
+            Container(
+              height: 55,
+              width: 55,
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle, color: Color(widget.mainColor)),
+            ),
+          ],
+        ));
   }
 }
